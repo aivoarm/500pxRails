cf1cbbddba61b955c5daaaf09db6ac41ba856784
MAC <MAC@MAC.local>
2017-10-15 16:48:32 -0400

    500px on Ruby

--- app/controllers/pictures_controller.rb
+++ app/controllers/pictures_controller.rb
@@ -1,12 +1,53 @@
 class PicturesController < ApplicationController
-  def index
 
-  @page = 2
-  @feature = 'popular'
-  @image_size=1080
-  @url = 'https://500px.com'
-  @pics =   Picture.get("https://api.500px.com/v1/photos/?feature=#{@feature}&page=#{@page}&image_size=#{@image_size}")['photos']
+private def getApi(feature = "popular", page = 1  )
+    @@api =   Picture.get("https://api.500px.com/v1/photos/?feature=#{feature}&page=#{page}&image_size=1080")
+end
 
+
+def api
+
+    feature = params[:feature]
+    page = params[:current_page]
+    image_size = 1080#params[:image_size]
+
+    @pics = getApi(feature, page) #Picture.get("https://api.500px.com/v1/photos/?feature=#{@feature}&page=#{@page}&image_size=#{@image_size}")#['photos']
+    #@pics =  Picture.get("https://api.500px.com/v1/photos/?feature=#{feature}&page=#{page}&image_size=1000")#['photos']
+
+    render json: @pics
+
+ end
+
+
+  def categories
+    
+  page = params[:current_page]
+  feature = params[:feature]
+    # @image_size=1080#params[:image_size]
+    # @url = 'https://500px.com'
+    #@pics =  api[@pics]#Picture.get("https://api.500px.com/v1/photos/?feature=#{@feature}&page=#{@page}&image_size=#{@image_size}")['photos']
+    @pics = getApi(feature, page)['photos']
+
+
+    @category=[]
+      @pics.each do |chr|
+        @category.push(chr['category'].to_s)
+      end
+
+    @categories_list={}
+      File.open("public/500cat.txt", "r") do |f|
+        f.each_line do |line|
+          @categories_list[line.chomp.split[0]]=line.chomp.split[1]
+        end
+      end
+
+      @categories={}
+      @category.each do |e|
+        @categories[e] =@categories_list[e]
+      end
+
+
+      render json: @categories
 
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,10 +2,21 @@ Rails.application.routes.draw do
 
   get 'pictures/index'
 
+  get 'pictures/index:id' => 'pictures#index'
+
   get 'courses/index'
 
   root "angular#index"
-  get '/about' => 'angular#about'
+  get 'pictures/api' => 'pictures#api', :defaults => { :format => 'json' }, as: :query
+
+
+
+  get 'pictures/categories' => 'pictures#categories', :defaults => { :format => 'json' }
+
+
+  #get 'pictures/api:id' => 'pictures#api'
+
+  #resources :pictures, :defaults => { :format => 'json' }
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
